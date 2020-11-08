9e96191a5a49b2ff20199f9c4ebe664b0f59673c
Stefan Hauser <stefan@Stefans-MacBook-Pro.local>
2020-11-08 16:12:40 +0100

    Python Rest Call

--- eg/bot.py
+++ eg/bot.py
@@ -1,4 +1,6 @@
 from rivescript import RiveScript
+import requests
+url = 'https://api.covid19api.com/country/germany?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z'
 import os.path
 
 file = os.path.dirname(os.path.abspath("__file__"))
@@ -9,10 +11,14 @@ bot.load_directory(brain)
 bot.sort_replies()
 
 while True:
+	
 	msg = str(input('You> '))
 
 	if msg == 'q':
 		break
 	
+	if msg == 'Corona':
+		response = requests.get(url)
+		print(response.json())
 	else:
 		print('Bot>'+bot.reply('local user', msg))
